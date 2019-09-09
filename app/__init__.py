b0dec0f3c3c99b7d18df08359fa4733d89f6abe9
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2019-09-08 20:09:52 -0700

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, current_app
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+
+from app import models
